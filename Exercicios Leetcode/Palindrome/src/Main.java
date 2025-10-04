87d8d638f9ea03f7ca10a552cab5f6943fc9d9bc
Victor Henrique <102931817+VictorHp21@users.noreply.github.com>
2025-10-04 00:03:09 -0300

    Palindrome

---
+++ Exercicios Leetcode/Palindrome/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Vetor size: ");
+        int n = s.nextInt();
+
+        int[] num = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            num[i] = s.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (isPalindrome(num) == true) {
+                System.out.println("Yes, it a palindrome");
+                break;
+            } else {
+                System.out.println("isnt a palindrome");
+                break;
+            }
+        }
+
+    }
+
+    public static boolean isPalindrome(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != nums[nums.length - 1 - i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
